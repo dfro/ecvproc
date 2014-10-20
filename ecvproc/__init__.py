f058c2deedd6c097c8d9bff3d83a0c90fc4851b5
Dmitry Frolov <frolovds@gmail.com>
2014-10-20 16:16:36 +0400

    description added

--- ecvproc/__init__.py
+++ ecvproc/__init__.py
@@ -1,1 +1,22 @@
-from ecvproc import *
+"""
+=======
+ECVProc
+=======
+ECVProc is Python module for processing ECVPro output files.
+
+
+=========== ======================================================
+ functions
+=========== ======================================================
+cv_read      Read Capacitance-Voltage data from *.CV file 
+iv_read      Read Current-Voltage data from *.IV file
+ep_read      Read measured doping profile from *.EP file
+log_read     Read data from ecv log file
+lin_fit      Returns linear fit for measured 1/C^2 and calculated Doping level 
+
+=========== ======================================================
+
+"""
+
+
+from ecvproc import cv_read, iv_read, ep_read, log_read, lin_fit
